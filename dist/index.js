52c30d4ffacfcbf4c729f47627a6b60d4fcad3bb
Anurag Singh <96870454+AnuragSingh2511@users.noreply.github.com>
2026-01-27 20:39:07 +0530

    refactor: folder correction

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(person) {
-    return `Hello ${person}, Welcome`;
-}
-const username = "Chai aur Typescript";
-console.log(username);
-
-//# sourceMappingURL=index.js.map
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-function greet(person: string): string {
-    return `Hello ${person}, Welcome`
-}
-
-const username: string = "Chai aur Typescript"
-console.log(username);
